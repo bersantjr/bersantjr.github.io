6ae2b0868b4bc7ffa9a27754755787d192c3e7dc
bersantjr <90532820+bersantjr@users.noreply.github.com>
2024-04-15 17:45:00 -0300

    Add files via upload

---
+++ massilia/aplicaCarrossel.js
@@ -0,0 +1,11 @@
+/* Função para ser usada para habilitar e ativar o carrossel de fotos */
+function aplicaCarrossel(){
+        var habilitaCarrossel = $("#carrossel .habilitaCarrossel");
+        var carrosselProx   = habilitaCarrossel.next().length ? habilitaCarrossel.next() : $("#carrossel li:first");
+        carrosselProx.addClass("habilitaCarrossel").fadeIn();
+        habilitaCarrossel.removeClass("habilitaCarrossel");
+                }
+    $(function(){
+		$("#carrossel li:first");
+         window.setInterval( "aplicaCarrossel()", 1500 );
+         });
